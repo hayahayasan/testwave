59a01e5281a0bc9785e0ed43d6dc8da1dcb300c7
hayamar_x <73538253+hayahayasan@users.noreply.github.com>
2024-07-10 15:15:33 +0900

    first test

---
+++ App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import logo2 from './YYT.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is  a test!1!!!!!!11!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+              </a>
+
+              <img src={logo2} className="gaming" alt="logo" />
+
+
+              <iframe src="https://scratch.mit.edu/projects/1046218608/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
